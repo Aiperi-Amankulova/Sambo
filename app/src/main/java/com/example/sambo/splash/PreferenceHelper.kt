f2a8915229007028f0b88e1aeb86872e4a08336c
Aiperi <aip.amankulova@gmail.com>
2020-11-16 14:06:55 +0600

    New features for splash

--- app/src/main/java/com/example/sambo/splash/PreferenceHelper.kt
+++ app/src/main/java/com/example/sambo/splash/PreferenceHelper.kt
@@ -1,18 +1,20 @@
-package com.example.sambo
+package com.example.sambo.splash
 
 import android.content.Context
 import android.content.SharedPreferences
 
 object PreferenceHelper {
-    private const val  PREFERENCE_NAME = "Fitness_preference"
+    private const val PREFERENCE_NAME = "Fitness_preference"
     private const val IS_FIRST_LAUNCH = "IS_FIRST_LAUNCH"
 
     private var preference: SharedPreferences? = null
-    fun  initPreference(context: Context){
+    fun initPreference(context: Context) {
         preference = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE)
     }
-    fun setIsFirstLaunch(){
+
+    fun setIsFirstLaunch() {
         preference?.edit()?.putBoolean(IS_FIRST_LAUNCH, false)?.apply()
     }
-    fun getIsSecondLaunch() =  preference?.getBoolean(IS_FIRST_LAUNCH,true) ?:true
+
+    fun getIsSecondLaunch() = preference?.getBoolean(IS_FIRST_LAUNCH, true) ?: true
 }
--- app/src/main/java/com/example/sambo/splash/SplashActivity.kt
+++ app/src/main/java/com/example/sambo/splash/SplashActivity.kt
@@ -1,27 +1,28 @@
-package com.example.sambo
+package com.example.sambo.splash
 
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import com.example.sambo.R
 import com.example.sambo.ui.MainActivity
+import com.example.sambo.ui.on_boarding.OnBoardingActivity
 
-class Splashactivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         Handler().postDelayed({
             selectActivity()
-        },4000)
+        }, 4000)
 
     }
 
-    private fun selectActivity(){
-        if (PreferenceHelper.getIsSecondLaunch()){
-            startActivity(Intent(applicationContext, MainActivity:: class.java))
+    private fun selectActivity() {
+        if (PreferenceHelper.getIsSecondLaunch()) {
+            startActivity(Intent(applicationContext, OnBoardingActivity::class.java))
             finish()
-        }
-        else {
+        } else {
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
