b89255997f3f292cbaa354ac9f92e34d4309e1f9
Aiperi <aip.amankulova@gmail.com>
2020-11-16 10:43:30 +0600

    Files for on boarding

---
+++ app/src/main/java/com/example/sambo/ui/on_boarding/OnBoardingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sambo.ui.on_boarding
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.sambo.R
+
+class OnBoardingActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.page_on_boarding)
+    }
+   }
