5e7b25b1d1ed4c55d711af68c6fd7e0f792ec604
Aiperi <aip.amankulova@gmail.com>
2020-11-16 14:06:13 +0600

    Bug fixes in on boarding

--- app/src/main/java/com/example/sambo/ui/on_boarding/OnBoardingActivity.kt
+++ app/src/main/java/com/example/sambo/ui/on_boarding/OnBoardingActivity.kt
@@ -1,12 +1,64 @@
 package com.example.sambo.ui.on_boarding
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
 import com.example.sambo.R
+import com.example.sambo.data.DataOnBoard
+import com.example.sambo.ui.MainActivity
+import kotlinx.android.synthetic.main.page_on_boarding.*
 
-class OnBoardingActivity: AppCompatActivity() {
+class OnBoardingActivity : AppCompatActivity() {
+    private val list = arrayListOf<Fragment>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.page_on_boarding)
+        setupViewPager()
+        setupListener()
     }
-   }
+
+    private fun setupListener() {
+        vp_page.setOnPageChangeListener(object  : ViewPager.OnPageChangeListener{
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {}
+
+            override fun onPageSelected(position: Int) {
+                if (checktoPage(position)){
+                    btn_go.text = "Продолжить"
+                } else {
+                    btn_go.text = "Пропустить"
+                }
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {}
+        })
+
+        btn_go.setOnClickListener {
+            if (checktoPage(vp_page.currentItem)){
+                startActivity(Intent(this, MainActivity:: class.java))
+                finish()
+            } else{
+                vp_page.currentItem += 1
+            }
+        }
+    }
+
+    private fun checktoPage(position : Int) = position == list.size -1
+
+    private fun setupViewPager() {
+        val adapterl = OnBoardingAdapter(supportFragmentManager)
+        vp_page.adapter = adapterl
+        list.add(OnBoardingFragment.getInstance(DataOnBoard(R.drawable.first_on_board,getString(R.string.first_text_in_onboard))))
+        list.add(OnBoardingFragment.getInstance(DataOnBoard(R.drawable.second_on_board,getString(R.string.second_text_in_onboard))))
+        list.add(OnBoardingFragment.getInstance(DataOnBoard(R.drawable.third_on_board,getString(R.string.third_text_in_onboard))))
+        adapterl.update(list)
+        slide.setupWithViewPager(vp_page)
+
+    }
+}
---
+++ app/src/main/java/com/example/sambo/ui/on_boarding/OnBoardingFragment.kt
@@ -0,0 +1,44 @@
+package com.example.sambo.ui.on_boarding
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.sambo.R
+import com.example.sambo.data.DataOnBoard
+import kotlinx.android.synthetic.main.view_on_boarding.*
+
+class OnBoardingFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.view_on_boarding, container , false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViews()
+    }
+
+    private fun setupViews() {
+        val data = arguments?.get(DATA_ID) as DataOnBoard
+        first_tv.text = data.text
+        img.setImageResource(data.image)
+
+    }
+
+    companion object{
+        const val DATA_ID = "DATA_ID"
+
+        fun getInstance(data: DataOnBoard):OnBoardingFragment{
+            val fragment = OnBoardingFragment()
+            val bundle = Bundle()
+            bundle.putParcelable(DATA_ID,data)
+            fragment.arguments = bundle
+            return fragment
+        }
+    }
+}
